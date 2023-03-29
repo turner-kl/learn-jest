2d7fe54620dda25ffed3dd3feac9e605c53f9138
turner-kl <angra5296@gmail.com>
2023-03-30 08:15:05 +0900

    e2e

--- src/chapter2/ui/buton.test.tsx
+++
@@ -1,9 +0,0 @@
-import renderer from "react-test-renderer";
-import { Button } from "./Button";
-
-describe("Button", () => {
-  it("renders correctly", () => {
-    const button = renderer.create(<Button />);
-    expect(button.toJSON()).toMatchSnapshot();
-  });
-});
